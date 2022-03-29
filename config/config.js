9834653683e0407730d8ab52543b58e351fa0c4c
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-29 17:28:58 -0500

    first commit

---
+++ config/config.js
@@ -0,0 +1,42 @@
+// exports.creds = {
+//   identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+
+//   clientID: 'ee372626-5658-4428-acf0-d5c48c4b969e',
+
+//   clientSecret: 'd5b01ac5-fa48-4d34-a4f5-1a71368122ce',
+
+//   responseType: 'code id_token',
+
+//   responseMode: 'form_post',
+
+//   redirectUrl: 'http://localhost:2121/auth/google',
+
+//   allowHttpForRedirectUrl: true,
+
+//   validateIssuer: false,
+
+//   issuer: null,
+
+//   passReqToCallback: false,
+
+//   useCookieInsteadOfSession: false,
+
+
+//   scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
+
+//   loggingLevel: false,
+
+//   nonceLifetime: null,
+
+//   nonceMaxAmount: 5,
+
+//   clockSkew: null,
+// };
+
+// exports.destroySessionUrl = 'http://localhost:2121';
+
+// exports.useMongoDBSessionStore = false;
+
+// exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
+
+// exports.mongoDBSessionMaxAge = 24 * 60 * 60;
---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+router.get('/google', passport.authenticate('google', {scope: ['profile']}
+))
+
+router.get('/google/callback', passport.authenticate('google', {
+  failureRedirect:'/'}), (req,res) => {
+    res.redirect('/todos')
+})
+
+router.post('/openid/return',
+  function(req, res, next) {
+    passport.authenticate('google/callback',
+      {
+        response: res,
+        failureRedirect: '/'
+      }
+    )(req, res, next)
+  },
+  function(req, res){
+    console.log('We received a return from Google')
+    res.redirect('/todos')
+  }
+)
+
+router.get('/logout', (req, res) => {
+  req.session.destroy(function(err) {
+    req.logOut()
+    res.redirect('/')
+  })
+})
+
+module.exports = router
