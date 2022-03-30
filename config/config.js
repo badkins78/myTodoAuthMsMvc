db4dde4388b4fc2486fb95f4e1dbe9ec22a1b140
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-30 08:52:40 -0500

    config.js change

--- config/config.js
+++ config/config.js
@@ -1,42 +1,19 @@
-// exports.creds = {
-//   identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+exports.creds = {
+  "client_id":"1018870726313-opakrms4g289kgqiirr73n8ca9hp0fmn.apps.googleusercontent.com",
+  "project_id":"mvctodomsauth",
+  "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+  "token_uri":"https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+  "client_secret":"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",
+  "redirect_uris":["https://mytodooauth20mvc.herokuapp.com/auth/google/callback", "http://localhost:2121"],
+  "javascript_origins":["https://mytodooauth20mvc.herokuapp.com"]
+}
+;
 
-//   clientID: 'ee372626-5658-4428-acf0-d5c48c4b969e',
+exports.destroySessionUrl = 'http://localhost:2121';
 
-//   clientSecret: 'd5b01ac5-fa48-4d34-a4f5-1a71368122ce',
+exports.useMongoDBSessionStore = false;
 
-//   responseType: 'code id_token',
+exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
 
-//   responseMode: 'form_post',
-
-//   redirectUrl: 'http://localhost:2121/auth/google',
-
-//   allowHttpForRedirectUrl: true,
-
-//   validateIssuer: false,
-
-//   issuer: null,
-
-//   passReqToCallback: false,
-
-//   useCookieInsteadOfSession: false,
-
-
-//   scope: ['profile', 'offline_access', 'https://graph.microsoft.com/mail.read'],
-
-//   loggingLevel: false,
-
-//   nonceLifetime: null,
-
-//   nonceMaxAmount: 5,
-
-//   clockSkew: null,
-// };
-
-// exports.destroySessionUrl = 'http://localhost:2121';
-
-// exports.useMongoDBSessionStore = false;
-
-// exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
-
-// exports.mongoDBSessionMaxAge = 24 * 60 * 60;
+exports.mongoDBSessionMaxAge = 24 * 60 * 60;
