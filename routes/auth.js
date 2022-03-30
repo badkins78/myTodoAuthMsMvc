182c2f400b8599e4507c4c123555199bc09f2876
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-30 09:11:05 -0500

    routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,31 +5,29 @@ const router = express.Router()
 router.get('/google', passport.authenticate('google', {scope: ['profile']}
 ))
 
-router.get('auth/google/callback', passport.authenticate('google', {
+router.get('/google/callback', passport.authenticate('google', {
   failureRedirect:'/'}), (req,res) => {
-    res.redirect('/todos')
+    res.redirect('/')
 })
 
-router.post('/openid/return',
-  function(req, res, next) {
-    passport.authenticate('auth/google/callback',
-      {
-        response: res,
-        failureRedirect: '/'
-      }
-    )(req, res, next)
-  },
-  function(req, res){
-    console.log('We received a return from Google')
-    res.redirect('/todos')
-  }
-)
+// router.post('/openid/return',
+//   function(req, res, next) {
+//     passport.authenticate('auth/google/callback',
+//       {
+//         response: res,
+//         failureRedirect: '/'
+//       }
+//     )(req, res, next)
+//   },
+//   function(req, res){
+//     console.log('We received a return from Google')
+//     res.redirect('/todos')
+//   }
+// )
 
 router.get('/logout', (req, res) => {
-  req.session.destroy(function(err) {
     req.logOut()
     res.redirect('/')
   })
-})
 
 module.exports = router
