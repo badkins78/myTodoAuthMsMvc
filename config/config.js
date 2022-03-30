3b15aa71b8d9c6636b182a30f37f3cff062aa441
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-30 10:06:27 -0500

    config.env, config.js, passport.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,47 @@
-// exports.creds = {
-//   client_id:"1018870726313-opakrms4g289kgqiirr73n8ca9hp0fmn.apps.googleusercontent.com",
-//   project_id:"mvctodomsauth",
-//   auth_uri:"https://accounts.google.com/o/oauth2/auth",
-//   token_uri:"https://oauth2.googleapis.com/token",
-//   auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
-//   client_secret:"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",
-//   redirect_uris:["https://mytodooauth20mvc.herokuapp.com/auth/google/callback", "http://localhost:2121"],
-//   javascript_origins:["https://mytodooauth20mvc.herokuapp.com"]
-// }
-// ;
+exports.creds = {
+  clientID:
+    "1018870726313-opakrms4g289kgqiirr73n8ca9hp0fmn.apps.googleusercontent.com",
 
-// exports.destroySessionUrl = 'http://localhost:2121';
+  clientSecret: "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",
 
-// exports.useMongoDBSessionStore = false;
+  redirectUrl: [
+    "https://mytodooauth20mvc.herokuapp.com/auth/google/callback",
+    "http://localhost:2121",
+  ],
 
-// exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
+  projectID: "mvctodomsauth",
 
-// exports.mongoDBSessionMaxAge = 24 * 60 * 60;
+  validateIssuer: false,
+
+  issuer: null,
+
+  passReqToCallback: false,
+
+  useCookieInsteadOfSession: false,
+
+  scope: "profile",
+
+  loggingLevel: false,
+
+  nonceLifetime: null,
+
+  nonceMaxAmount: 5,
+
+  clockSkew: null,
+
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+
+  tokenUri: "https://oauth2.googleapis.com/token",
+
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+
+  javascript_origins: ["https://mytodooauth20mvc.herokuapp.com"],
+};
+
+exports.destroySessionUrl = "http://localhost:2121";
+
+exports.useMongoDBSessionStore = false;
+
+exports.databaseUri = "mongodb://localhost/OIDCStrategy";
+
+exports.mongoDBSessionMaxAge = 24 * 60 * 60;
--- config/passport.js
+++ config/passport.js
@@ -1,43 +1,58 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const mongoose = require('mongoose')
-const User = require('../models/User')
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const config = require("../config/config");
+const User = require("../models/User");
 
 module.exports = function (passport) {
   passport.use(
-    new GoogleStrategy({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // callbackURL: "/auth/google/callback"
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const newUser = {
-        googleId: profile.id,
-        displayName: profile.displayName,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        image: profile.photos[0].value
-      }
+    new GoogleStrategy(
+      {
+        clientID: config.creds.clientID,
+        clientSecret: config.creds.clientSecret,
+        redirectUrl: config.creds.redirectUrl,
+        projectID: config.creds.projectID,
+        validateIssuer: config.creds.validateIssuer,
+        issuer: config.creds.issuer,
+        passReqToCallback: config.creds.passReqToCallback,
+        scope: config.creds.scope,
+        loggingLevel: config.creds.loggingLevel,
+        nonceLifetime: config.creds.nonceLifetime,
+        nonceMaxAmount: config.creds.nonceMaxAmount,
+        useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+        authUri: config.creds.authUri,
+        tokenUri: config.creds.tokenUri,
+        auth_provider_x509_cert_url: config.creds.auth_provider_x509_cert_url,
+        javascript_origins: config.creds.javascript_origins,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        };
 
-      try{
-        let user = await User.findOne({googleId: profile.id})
-        if(user) {
-          done(null, user)
-        } else {
-          user = await User.create(newUser)
-          done(null, user)
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
+        } catch (err) {
+          console.error(err);
         }
-      } catch(err){
-        console.error(err)
       }
-    }
-  )
-)
+    )
+  );
 
-passport.serializeUser((user, done) => {
-  done(null, user.id)
-})
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => done(err, user))
-})
-}
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
+};
