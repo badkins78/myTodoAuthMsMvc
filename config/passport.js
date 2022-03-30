ae46d158031741cd529f6d1b3925bf00c0a96dcd
badkins78 <72710121+badkins78@users.noreply.github.com>
2022-03-30 09:16:59 -0500

    config.js, .env, passport.js

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
     new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: process.env.REDIRECT_URIS
     },
     async (accessToken, refreshToken, profile, done) => {
       const newUser = {
